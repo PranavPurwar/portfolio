b45c08d6f966b0f1dbdac88ba180b67bda5361a2
PranavPurwar <purwarpranav80@gmail.com>
2023-07-15 01:07:44 +0530

    Initial Commit

---
+++ constants/data.js
@@ -0,0 +1,67 @@
+const userData = {
+  githubUsername: "PranavPurwar",
+  name: "Pranav Purwar",
+  designation: "Android Developer",
+  avatarUrl: "https://avatars.githubusercontent.com/u/75154889?v=4",
+  email: "purwarpranav80@gmail.com",
+  projects: [
+    {
+      title: "Cosmic IDE Rewrite",
+      link: "https://github.com/Cosmic-IDE/rewrite",
+      imgUrl: "/ide.svg",
+    },
+    {
+      title: "Cosmic IDE",
+      link: "https://github.com/Cosmic-IDE/Cosmic-IDE",
+      imgUrl: "https://github.com/Cosmic-Ide/Cosmic-Ide/blob/dev/fastlane/metadata/android/en-US/images/icon.png?raw=true",
+    },
+    {
+      title: "Dependency Resolver",
+      link: "https://github.com/Cosmic-IDE/DependencyResolver",
+      imgUrl: "/dependency.png",
+    },
+    {
+      title: "PLJ Language",
+      link: "https://github.com/PranavPurwar/PLJ-Language",
+      imgUrl: "/plj.png",
+    },
+    {
+      title: "Memes - Reddit Meme Scraper",
+      link: "https://github.com/PranavPurwar/Meme",
+      imgUrl: "/meme.png",
+    }
+  ],
+  about: {
+    title:
+      "I am a self-taught software developer specializing in Android Development and IDE Development.",
+    description: [
+      `I've been developing android application since I was 11 years old. I started tinkering around the programming space in late 2018. At that time, I learnt python for the sake of creating a discord bot.`,
+      `Then I came to know about the Sketchware Pro community and started developing my own IDE for learning programming called Cosmic IDE. As of now, It's my main project and I'm working on it full time.`,
+    ],
+    currentProject: "Cosmic IDE Rewrite",
+    currentProjectUrl: "https://github.com/Cosmic-IDE/rewrite",
+  },
+  experience: [
+    {
+      title: "Founder and Main Developer",
+      company: "Cosmic IDE",
+      year: "2020",
+      companyLink: "https://github.com/Cosmic-IDE",
+      desc: "I basically built the whole IDE from scratch. It's a full fledged IDE for Android with support for Java and Kotlin languages.",
+    },
+    {
+      title: "Software Developer",
+      company: "Multiple",
+      companyLink: "https://github.com/PranavPurwar",
+      desc: "I don't really wanna list all my contributions here. You can check them out on my github profile.",
+    }
+  ],
+  socialLinks: {
+    instagram: "https://instagram.com/pranavpurwar",
+    twitter: "https://twitter.com/Pranav0880",
+    linkedin: "https://linkedin.com/in/ranavpurwar",
+    github: "https://github.com/PranavPurwar",
+  },
+};
+
+export default userData;
